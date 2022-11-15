bc7c3aa3ae2f6b8cee8fb7765a694256c21f11e8
KimSeongHwan <89847975+PotatoMeme@users.noreply.github.com>
2022-11-15 12:27:02 +0900

    Feat "ip test"

---
+++ app/src/main/java/com/collegecapstoneteam1/cookingapp/data/model/sever/Meta.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.test
+
+data class Meta(
+    val end: Boolean,
+    val pageable_count: Int,
+    val total_count: Int
+)
---
+++ app/src/main/java/com/collegecapstoneteam1/cookingapp/data/model/sever/ServerResponse.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.test
+
+data class ServerResponse(
+    val meta: Meta,
+    val values: List<Value>
+)
